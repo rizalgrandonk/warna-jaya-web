5f0741e1ffb5b574439f45b3f99d679a78ef9cba
ja'far shodiq <jsshodiq@gmail.com>
2022-01-09 00:07:04 +0700

    fixing hero image

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import HeroImage from "../public/images/bg-1.jpg"
 export default function Home() {
   return (
     <>
-      <div className="w-full h-[130vh] relative">
+      <div className="w-full h-screen md:h-[140vh] relative">
         <Image 
           src={HeroImage}
           alt=""
@@ -19,11 +19,12 @@ export default function Home() {
 
           
         </div>
-      </div>
-      <div className="h-screen relative">
-        <svg className="absolute -top-64" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <svg className="hidden md:block absolute bottom-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path fill="#f3f4f6" fillOpacity="1" d="M0,96L120,133.3C240,171,480,245,720,240C960,235,1200,149,1320,106.7L1440,64L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
         </svg>
+      </div>
+      <div className="h-screen relative">
+        
       </div>
     </>
   )
